30ad08e4a1ce6db2c418faf0c5727790379f37c9
Miffuru <miffakhril@gmail.com>
2026-03-19 05:43:38 +0800

    Create library.js

---
+++ library.js
@@ -0,0 +1,16 @@
+const miffLibrary = {
+  
+  // BOOK 1: Fiqh Muamalat
+  "fiqh-muamalat": [
+    { title: "Chapter 5: Concept Of Contract", link: "https://miffpustaka.blogspot.com/2026/01/fm-chapter-5-concept-of-contract-aqd.html" },
+    { title: "Chapter 6: Sale-Based Transactions 1", link: "https://miffpustaka.blogspot.com/2026/01/fm-chapter-6-sale-based-transactions-1.html" }
+    // Add more here...
+  ],
+
+  // BOOK 2: Business Law
+  "business-law": [
+    { title: "Chapter 1: Intro to Law", link: "#" }
+  ]
+  
+  // You can add up to 100 books here safely!
+};
